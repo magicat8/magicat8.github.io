9c2ee36e3c40f2a3cebca3c7e01e72439ae9e890
magicat8 <magicat8bill@gmail.com>
2025-03-05 11:17:20 -0700

    made changes

--- script.py
+++
@@ -1,9 +0,0 @@
-from browser import document, alert
-
-def submit_data(event):
-    name_input = document["name"].value  # Get the value of the input field
-    document["output"].text = f"Hello, {name_input}!"  # Display the personalized greeting
-    alert("Button clicked!")  # Debugging alert
-
-# Bind the submit_data function to the button click event
-document["submit-button"].bind("click", submit_data)
